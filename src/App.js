59894b9a4383bd63d12bd59eb5076904ff5aa914
ashokmlr <53170124+ashokmlr@users.noreply.github.com>
2022-07-25 15:58:40 +0530

    Removing logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import REACT from "react"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <h1>Website is under maintanance</h1>
        
       </header>
