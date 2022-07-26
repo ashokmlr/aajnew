53442d60ea5911ccd31d535dd1cb16e75a13f9e1
unknown <ashokkumardev26@gmail.com>
2022-07-26 17:54:28 +0530

    code changes for responsive

--- src/headerImage/headerImage.js
+++ src/headerImage/headerImage.js
@@ -5,12 +5,14 @@ import "./headerIm.css"
 export const HeaderImage = () => {  
     return(
         <>
-         <div className="hdim">
+        <div>
             <ul>
                 <li id="ttl">aajnew</li>
                 <li id="abt">About Us</li>
                 <li>Contact</li>
             </ul>
+        </div>
+         <div className="hdim">
             <img src={hImage} alt = "text" width="100%" height = "500px" />
             <h1>Create your Quote</h1>
          </div>
