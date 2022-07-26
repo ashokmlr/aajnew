5452ca8d6c3f46d39ce8ccdaea7dcf916ef9c12e
unknown <ashokkumardev26@gmail.com>
2022-07-26 11:39:23 +0530

    Header Image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import REACT from "react"
+import { HeaderImage } from "./headerImage/headerImage";
+import "./App.css"
 
 function App() {
   return (
-    <div>
-        <h1>Website is under maintanance</h1>
-    </div>
+    <>
+     <HeaderImage/>
+    </>
   );
 }
 
